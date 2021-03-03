6d4630c781302e51314fbb4b98ccb155308c9ee2
Sy Rashid <sy.rashid.11@gmail.com>
2021-03-03 08:18:28 -0500

    FEAT: Added elastic search

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,7 @@
 class MoviesController < ApplicationController
   def index
     if params[:query].present?
-      @movies = Movie.where("title ILIKE ?", "%#{params[:query]}%")
+      @movies = Movie.search_by_title_and_synopsis(params[:query])
     else
       @movies = Movie.all
     end
